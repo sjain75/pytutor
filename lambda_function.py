8e9c89fc15e50fa2c864a74c2b9e0f9e48018529
Sarwagya Jain <sjain75@wisc.edu>
2020-03-07 18:14:04 -0600

    Pytutor Files Separated Out of Tyler's repo

---
+++ lambda_function.py
@@ -0,0 +1,118 @@
+#Probably not needed if we transition out of AWS
+
+
+# Copyright 2018 Tyler Caraza-Harter
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import json, urllib, boto3, botocore, base64, time, traceback, random, string, copy
+from collections import defaultdict as ddict
+
+from lambda_framework import *
+from projects import *
+from extensions import *
+from roster import *
+from messages import *
+from pytutor import *
+
+def lambda_handler(event, context):
+    # TODO: we'll need to specify this in all calls from the front end...
+    course = event.get('course', 'a')
+    if not course in ('a', 'b', 'c'):
+        return error('invalid course ID: "%s"' % course)
+    init_s3(course)
+
+    ts0 = datetime.datetime.utcnow().timestamp()
+
+    # identify user
+    try:
+        user = get_user(event)
+    except Exception as e:
+        user = None
+    #if user != None:
+        #save_user_info(user)
+
+    # try to invoke the function
+    fn = ROUTES.get(event['fn'], None)
+    if fn != None:
+        try:
+            # if a check fails, it will raise an exception
+            for checker in EXTRA_AUTH[event['fn']]:
+                checker(user)
+
+            code, data = fn(user, event)
+            result = {
+                "isBase64Encoded": False,
+                "statusCode": code,
+                "headers": {},
+                "body": data
+            }
+        except Exception as e:
+            result = error('Exception: '+str(e) + ' '+traceback.format_exc())
+    else:
+        result = error('no route for '+event['fn'])
+
+    # try to log the event
+    ts1 = datetime.datetime.utcnow().timestamp()
+    try:
+        record = log_record(event, result, user, ts0, ts1)
+ #       firehose().put_record(DeliveryStreamName=FIREHOSE,
+                             # Record = {'Data': json.dumps(record) + "\n"})
+    except Exception as e:
+        result = error('Firehose Error: '+str(e) + ' '+traceback.format_exc())
+
+    return result
+
+
+def log_record(event, response, user, ts0, ts1):
+    record = {
+        'request': copy.deepcopy(event),
+        'response': copy.deepcopy(response),
+        'user_id': user.get('sub', None) if not user is None else None,
+        'ts0': ts0,
+        'ts1': ts1,
+    }
+
+    # we don't want traces to be too lange.
+    # Thus, we replace long strings anywhere in the recursive structure with None.
+    # we record these replacements with in dropped.
+    max_str_len = 64
+    dropped = []
+
+    def compact_structs(location, struct):
+        """location is a tuple of keys (keys in dicts or indices in lists) from the record
+        object to get to struct"""
+
+        if isinstance(struct, dict):
+            keys = struct.keys()
+        elif isinstance(struct, list):
+            keys = range(len(struct))
+        else:
+            return
+
+        for key in keys:
+            val = struct[key]
+            sublocation = location+(key,)
+            
+            if isinstance(val, str) and len(val) > max_str_len:
+                dropped.append({
+                    "location": sublocation,
+                    "length": len(val)
+                })
+                struct[key] = None
+            else:
+                compact_structs(sublocation, val)
+    compact_structs((), record)
+    record['dropped'] = dropped
+
+    return record
---
+++ pytutor.js
@@ -0,0 +1,178 @@
+$( document ).ready(function() {
+	$("[id=vcrControls]").children().attr("disabled",true);
+	
+	$(".lineNo").each(function(){
+		$(this).attr("style","cursor: pointer;")
+		$(this).text("🔘");
+	}); //replace all line numbers with something that looks more clickable
+	var obj;
+	var reload = function(){
+		username = $("span#useremail").text();
+
+		$(".manualQuestionDiv").each(function(){
+				parentDiv=$(this);
+				if(username!=""){
+				if(!($(parentDiv).attr("sent")=="updated")){
+					activity=($(parentDiv).prev().prev().attr("id"));
+					(common.callLambda({"fn":"isAnswered", questionCode:activity},
+						function (obj) {
+							
+						
+							  if( !('error' in obj) && obj["body"]!="false" ) {
+							     parentDiv = $("#"+obj["body"]).next().next();
+							      $(parentDiv).attr("sent","updated");
+							      $(parentDiv).find(".title-bar-chevron-container").html('<div aria-label="Activity completed" id="ember6016" class="zb-chevron title-bar-chevron orange large check filled ember-view"> </div>');
+							      $(parentDiv).find(".chevron-container").html('<div aria-label="Question completed" id="ember6023" class="zb-chevron question-chevron orange medium check filled ember-view"> </div>');
+							  }
+							  else {
+							      console.log(obj);
+							  }
+						}));   
+						
+				}
+			}
+		});
+
+	};
+
+	$(".reload").on("click",reload);
+	var noClick = function() {
+		parentDiv =$(this).parents("div").last(); //find the parentDiv of the question based on the step clicked on 
+		console.log(parentDiv);
+		var script = (parentDiv.next().text()); //get the trace value for this question
+		script = script.substring(0, script.search("addVisualizerToPage")); //get code that evaluateds trace array
+		eval(script); //evaulate script
+		currInstr = parseInt($("#curInstr", parentDiv).text().split(" ")[1]); //current step based on the visualizer
+
+		try{     
+                        currInstrLine = parseInt((trace["trace"])[currInstr-1]["line"]);//line that program is currently on
+                }catch(error){
+                        $(parentDiv).find("#answerTruth").remove();
+                        $(parentDiv).find("#dataViz").after("<div id=answerTruth style='font-size:40px'><b>Complete</b></div>");
+			return;
+                }
+	
+		nextInstrLine = parseInt((trace["trace"])[currInstr]["line"]); //line that program should go to next
+		if((trace["trace"])[currInstr]["event"]=="return"&&currInstrLine==nextInstrLine){
+			nextInstrLine=parseInt((trace["trace"])[currInstr+1]["line"]); //line that program should go to next
+		}
+		var currLine=this.id; //the line that user clicked on 
+		var answerTruth = (("lineNo"+(nextInstrLine))==currLine);
+		if(answerTruth)		
+		{
+			$(parentDiv).find("#vcrControls").children().attr("disabled",false);
+
+			$(parentDiv).find("#answerTruth").remove();
+			$(parentDiv).find("#dataViz").after("<div id=answerTruth style='font-size:40px'><img width='8%' style='padding-left:2%' src='correct-checkmark.svg'></img></div>");
+			var fwdButton = $(parentDiv).find("#jmpStepFwd");			
+						
+			var timeout = 0;			
+			while("lineNo"+currInstrLine!=currLine)
+			{	$(fwdButton).trigger("click");			
+				currInstr = parseInt($("#curInstr", parentDiv).text().split(" ")[1]);
+			
+
+				currInstrLine = parseInt((trace["trace"])[currInstr-1]["line"]);
+				timeout++;				
+				if(timeout==20)
+					break;
+			}	
+			$("[id=vcrControls]").children().attr("disabled",true);
+
+		}		
+		else{
+			$(parentDiv).find("#answerTruth").remove();
+			$(parentDiv).find("#dataViz").after("<div id=answerTruth><img width='8%' style='padding-left:2%' src='incorrect-cross.svg'></img></div>");		
+
+		}
+
+
+	}	
+
+	$(".lineNo").on("click", noClick);
+	var notLoggedInAttempt = false;
+	var manualCheck = function(){
+		parentDiv = $(this).closest(".manualQuestionDiv");
+		username = $("span#useremail").text();
+		if(username==""){
+			if(!notLoggedInAttempt){
+				notLoggedInAttempt = true;
+				$(parentDiv).append("<span id='notLoggedIn'>You are not logged in so none of these questions will be graded! But feel free to continue for practice! :)</span>");
+			}
+		}
+		else{
+			 username = $.trim(username);
+			if(notLoggedInAttempt){
+				$(parentDiv).children("#notLoggedIn").remove();
+				notLoggedInAttempt = false;
+			}
+		}
+		script = $(parentDiv).prev().text();
+		script = script.substring(script.search("manualAnswer")); //get code that evaluateds trace array
+		console.log(script);
+		eval(script); //evaulate script
+		userAnswer = $(parentDiv).find("#manualAnswer").val();
+		answerTruth = userAnswer==manualAnswer;
+		$(parentDiv.prev().prev()).find("#manualAnswerTruth").remove();
+
+		if(answerTruth){
+			$(parentDiv).find(".title-bar-chevron-container").html('<div aria-label="Activity completed" id="ember6016" class="zb-chevron title-bar-chevron orange large check filled ember-view"> </div>');
+			$(parentDiv).find(".chevron-container").html('<div aria-label="Question completed" id="ember6023" class="zb-chevron question-chevron orange medium check filled ember-view"> </div>');
+			if(username!=""){
+				if(!($(parentDiv).attr("sent")=="updated")){
+					activity=($(parentDiv).prev().prev().attr("id"));
+					console.log(common.callLambda({"fn":"addNewAnswer", questionCode:activity},
+						function (obj) {
+							  if( !('error' in obj) ) {
+							      console.log(obj);
+							      $(parentDiv).attr("sent","updated");
+							  }
+							  else {
+							      console.log(obj);
+							  }
+						    }
+						));
+				}
+			}
+		
+		}
+		else{
+			 $(parentDiv).find(".chevron-container").html("<img width='40%' style='padding-left:2%' src='incorrect-cross.svg'></img>");
+
+		}
+
+	};
+
+
+	questionHtmlString = '<div content_resource_id="32241403" class="manualQuestionDiv interactive-activity-container short-answer-content-resource participation large ember-view"><div class="activity-title-bar" data-ember-action="" data-ember-action-6015="6015"> <span aria-hidden="true" class="activity-type"> <div class="type">participation</div> <div class="label">activity</div> </span> <div class="separator"></div> <h6 class="activity-description"> <div class="activity-title">"""manualQuestionTitle"""</div><div class="title-bar-chevron-container"> </div> <!----></h6> </div> <div class="activity-payload"> <div class="activity-instructions"> </div> <div id="ember6017" class="content-resource short-answer-payload ember-view"> <div id="ember6019" class="question-set-question short-answer-question ember-view"><div class="question"> <div class="setup flex-row"> <div class="label">1)</div> <div class="text">"""manualQuestion"""</div> </div> <div class="question-container flex-row"> <div class="input"> <pre><textarea cols="20" rows="1" spellcheck="false" autocapitalize="false" id="manualAnswer" class="zb-text-area hide-scrollbar ember-text-area ember-view"></textarea></pre> <div class="buttons flex-row"> <button class="submitManual" class="check-button zb-button primary raised ember-view"><!----> <span class="title">Check</span> </button> <button id="ember6022" class="show-answer-button zb-button secondary ember-view"><!----> <span class="title">Show answer</span> </button> </div> </div> </div> </div> <div role="alert" aria-atomic="true" class="explanation "> <!----></div> <!----> <div class="chevron-container"> </div> </div> </div> <!----></div> </div>';
+
+	//Take the simple div with class manual question and make it fancier. Runs at page load for each div with class "manualQuestion"
+	$(".manualQuestion").each(function(){
+		manualQuestionString= $(this).text();
+		manualTitleString = $(this).prev().prev().prev().text(); 
+		console.log(manualTitleString);
+		privateQuestionHtml=questionHtmlString;
+		privateQuestionHtml = privateQuestionHtml.replace('"""manualQuestion"""',manualQuestionString);
+		privateQuestionHtml = privateQuestionHtml.replace('"""manualQuestionTitle"""',manualTitleString);
+
+		$(this).after(privateQuestionHtml);
+		$(this).remove();
+	});
+
+	//Attach Handler to element with class "submitManual" so that it runs function manualCheck on click
+	$(".submitManual").on("click",manualCheck);
+	
+	//Define function handler for  
+	$("#question").on('input',function(e){
+		var question = $(this).val();
+		console.log(question);
+		if(question.indexOf("line")!=-1){
+			console.log("True");
+
+		var script = (parentDiv.next().text());
+		script = script.substring(0, script.search("addVisualizerToPage"));
+		console.log(script);
+		eval(script);
+		}
+	});
+})
