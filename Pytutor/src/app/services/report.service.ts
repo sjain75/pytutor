3e10b864e5626d721eb578e27c189a0ad03d17c6
eric-zhou2001 <ericzhzhou2001@gmail.com>
2020-07-24 19:13:58 -0400

    iframe works, no console.log

--- Pytutor/src/app/services/report.service.ts
+++ Pytutor/src/app/services/report.service.ts
@@ -12,7 +12,6 @@ export class ReportService {
     var profUsername = <HTMLInputElement>document.getElementById("profUsername");
     var reportInputForm = document.getElementById("reportInputForm");
     reportpage.src = `http://pytutor.ddns.net/${profUsername.value}/pages/report.html`;
-    console.log(reportpage.src);
     reportpage.classList.remove("hidden");
     reportInputForm.classList.add("hidden");
   }
