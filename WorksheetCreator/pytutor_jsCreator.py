247718151c149778947d7576551f86b51a3cc37f
ericzhzhou2001@gmail.com <ericzhzhou2001@gmail.com>
2020-08-07 13:41:01 -0400

    Added config functionality + pytutor_worksheets dir in CWD

--- WorksheetCreator/pytutor_jsCreator.py
+++ WorksheetCreator/pytutor_jsCreator.py
@@ -16,6 +16,15 @@ answers = {}
 
 py = ""
 
+config = None
+
+# Will load config that the user provides in the pytutor_worksheets. Stores it in global
+# variable config as a dictionary.
+def loadConfig():
+    global config
+    with open('./pytutor_worksheets/config.json') as file:
+        config = json.load(file)
+
 # Converts python code to JavaScript. 
 # If the python file does not exist in specified file destination, will throw an error.
 # py - the file path.
@@ -29,11 +38,15 @@ def run_pytutor(py):
 def main():
     if len(sys.argv) >= 2:
         # Creates pytutor_worksheets directory in CWD.
-        try:
-            os.mkdir("./pytutor_worksheets")
-        except OSError:
-            print("Remove me! directory already exists...")
-            
+        if not os.path.exists("./pytutor_worksheets"):
+            try:
+                os.mkdir("./pytutor_worksheets")
+            except OSError:
+                sys.exit(1)
+        
+        if "config.json" in sys.argv:
+            loadConfig()
+
         createTracesPage()
         print("Creating your worksheet now...")
 
@@ -42,31 +55,36 @@ def main():
         # pre-existing file.
         append = False
         while(True):
-            fileName = checkEmptyInput("What is the file name?")
+            worksheetName = checkEmptyInput("What is the worksheet name?")
+            # Removes all whitespace from worksheetName, stores it into fileName variable
+            if " " in worksheetName:
+                fileName = worksheetName.replace(" ", "")
+            else:
+                fileName = worksheetName
+
             # Just a little QOL thing... It's also cuz I'm lazy and forget to type .html lol
             if ".html" not in fileName:
                 fileName += ".html"
-            if " " in fileName:
-                print("Error! No spaces allowed in file name.")
-            else:
-                if os.path.exists("./pytutor_worksheets/" + fileName):
-                    userChoice = input("This file already exists. Would you like to append it to the already existing file? (Y/N)")
+
+            # Actual worksheet creation.
+            if os.path.exists("./pytutor_worksheets/" + fileName):
+                userChoice = input("This file already exists. Would you like to append it to the already existing file? (Y/N)")
+                if userChoice.lower().strip() == "y":
+                    append = True
+                    break
+                elif userChoice.lower().strip() == "n":
+                    userChoice = input("Are you sure? By selecting yes, you will overwrite the pre-existing file. (Y/N)")
                     if userChoice.lower().strip() == "y":
-                        append = True
+                        os.remove("./pytutor_worksheets/" + fileName)
                         break
-                    elif userChoice.lower().strip() == "n":
-                        userChoice = input("Are you sure? By selecting yes, you will overwrite the pre-existing file. (Y/N)")
-                        if userChoice.lower().strip() == "y":
-                            os.remove("./pytutor_worksheets/" + fileName)
-                            break
-                        else:
-                            print("Unknown command!")
-                            continue
                     else:
                         print("Unknown command!")
                         continue
                 else:
-                    break
+                    print("Unknown command!")
+                    continue
+            else:
+                break
         
         # Actual part of worksheet creation
         global answers
@@ -82,12 +100,13 @@ def main():
             open("./pytutor_worksheets/" + fileName, 'x')
             answers = {"worksheetCode": fileName.replace(".html", ""), "totalNumOfQuestions": 0}
             try:
+                # MAKE SURE TO CHANGE THIS.
                 copyfile("../pages/defaultPageLayout.html", "./pytutor_worksheets/" + fileName)
             except IOError:
                 print("Error! Check to make sure \"defaultPageLayout.html\" exists within the pages directory")
                 sys.exit(1)
             
-            listOfFileLines = createWorksheetTitle(fileName, forTracesPage=False)
+            listOfFileLines = createWorksheetTitle(fileName, worksheetName, forTracesPage=False)
             listOfFileLines = generateScripts(forTracesPage=False, lines=listOfFileLines)
         
         # This will write to the original file.
@@ -118,7 +137,13 @@ def checkEmptyInput(inputString):
 # Returns the updated file in a list.
 def generateScripts(forTracesPage, lines):
     global py
-    for py in sys.argv[1:]:
+    if config:
+        pys = list(config.keys())
+        pys.remove("wsTitle")
+    else:
+        pys = sys.argv[1:]
+    
+    for py in pys:
         lines = addToFile([generateTrace(py, forTracesPage)], lines, not(forTracesPage))
     return lines
 
@@ -128,12 +153,13 @@ def generateScripts(forTracesPage, lines):
 def createTracesPage():
     print("Generating ONLY story/trace questions in html/trace.html...")
     try:
+        #MAKE SURE TO CHANGE THIS.
         copyfile("../pages/defaultPageLayout.html", "./pytutor_worksheets/trace.html")
     except IOError:
         print("Error! Check to make sure \"defaultPageLayout.html\" exists within the pages directory")
         sys.exit(1)
 
-    listOfFileLines = createWorksheetTitle(fileName="trace.html", forTracesPage=True)
+    listOfFileLines = createWorksheetTitle(fileName="trace.html", worksheetName="Trace File", forTracesPage=True)
 
     listOfFileLines = generateScripts(forTracesPage=True, lines=listOfFileLines)
 
@@ -148,20 +174,29 @@ def createTracesPage():
 # forTracesPage - boolean signaling whether or not we are generating the trace.html page.
 # return Code - returns the trace/div container.
 def generateTrace(py, forTracesPage):
+    global config
     js = run_pytutor(py)
     div = py.replace(".", "_").replace("/", "_")
     
     if not(forTracesPage):
-        print("Working on file " + py + "...")
-        while(True):
+        if config is not None and "stepNumber" in config[py]:
             try:
-                numSteps = (int(input("What step do you want the trace to begin at (an integer)?")) - 1)
+                numSteps = config[py]["stepNumber"]
                 if not isinstance(numSteps, int) or numSteps < 0:
                     raise ValueError
-                else:
-                    break
             except ValueError:
-                print("Error! Not a valid number! The step number must be above 0 and must be an integer!")
+                print("Error! Not a valid number! The step number must be above 0 and must be an integer! Check the config.json...")
+        else:
+            print("Working on file " + py + "...")
+            while(True):
+                try:
+                    numSteps = (int(input("What step do you want the trace to begin at (an integer)?")) - 1)
+                    if not isinstance(numSteps, int) or numSteps < 0:
+                        raise ValueError
+                    else:
+                        break
+                except ValueError:
+                    print("Error! Not a valid number! The step number must be above 0 and must be an integer!")
         
         start = "startingInstruction:" + str(numSteps)
         code = EMBEDDING.replace("DIV", div).replace("TRACE", js).replace("START", start)
@@ -175,7 +210,8 @@ def generateTrace(py, forTracesPage):
 # fileName - the name of the file.
 # forTracesPage - boolean signaling whether or not we are generating the trace.html page.
 # return lines - returns the updated file in the form of a list.
-def createWorksheetTitle(fileName, forTracesPage):
+def createWorksheetTitle(fileName, worksheetName, forTracesPage):
+    global config
     with open (("./pytutor_worksheets/" if forTracesPage else "./pytutor_worksheets/") + fileName) as file:
         lines = file.read().splitlines()
     
@@ -184,9 +220,13 @@ def createWorksheetTitle(fileName, forTracesPage):
         if x == "<h1 class=\"problem\">Template File, title would appear here!</h1>":
             break
         i += 1
-    
+
     if not(forTracesPage):
-        header = checkEmptyInput("What would you like to name your worksheet?")
+        if config is not None and "wsTitle" in config:
+            header = config["wsTitle"]
+        else:
+            header = worksheetName
+            
         header = "<h1 class = \"problem\">" + header + "</h1>"
         lines[i] = header
     else:
@@ -201,6 +241,7 @@ def createWorksheetTitle(fileName, forTracesPage):
 # listOfFileLines - the file that we are adding to in the form of a list.
 # headers - boolean, telling the program if we want to add additional headers/manual questions.
 def addToFile(codeList, listOfFileLines, headers):
+    global py, config
     indexOfComment = 0
     endTags = []
     for x in listOfFileLines:
@@ -216,17 +257,20 @@ def addToFile(codeList, listOfFileLines, headers):
 
     if headers:
         for scriptIndex in range(0, len(codeList)):
-            while(True):
-                try:
-                    wsProb = float(input("What worksheet problem is this (an int or float)?"))
-                except ValueError:
-                    print("Error! Not a valid worksheet problem.")
-                    continue
-                break
+            # Config Option
+            if config is not None and "problemName" in config[py]:
+                wsProb = config[py]["problemName"]
+            # Manual input option
+            else:
+                wsProb = checkEmptyInput("What is the problem title?")
+            # Appending wsProblem
             listOfFileLines.append("<h2 class = \"problem\">Worksheet Problem " + str(wsProb) + "</h2>" + "\n")
             listOfFileLines.append(codeList[scriptIndex])
             # This prompts the user for manual questions (if any).
-            listOfFileLines.append("\n" + generateQuestionString())
+            if config is not None and "manualQuestion" in config[py]:
+                listOfFileLines.append("\n" + generateQuestionStringConfig())
+            else:
+                listOfFileLines.append("\n" + generateQuestionString())
     else:
         for scriptIndex in range(0, len(codeList)):
             listOfFileLines.append(codeList[scriptIndex])
@@ -237,10 +281,32 @@ def addToFile(codeList, listOfFileLines, headers):
     listOfFileLines.extend(endTags)
     return listOfFileLines
 
+# Generates question strings if the config is specified/has manual questions to enter.
+# Returns - string that contains all the questions.
+def generateQuestionStringConfig():
+    global config, answers, py
+    divQuestions = ""
+    for manualQuestions in config[py]["manualQuestion"]:
+        question = manualQuestions["question"]
+        stepNumber = manualQuestions["stepNumber"]
+        answer = manualQuestions["answer"]
+
+        # Check if question at this step already exists.
+        if py.replace(".", "_") + "_" + str(stepNumber) in answers:
+            print("Question already exists!")
+            continue
+        else:
+            answers["totalNumOfQuestions"] += 1
+            answers[py.replace(".", "_") + "_" + str(stepNumber)] = answer
+
+        divQuestions += "\n<div step = " + str(stepNumber) + " class = \"manualQuestion\">" + question + "</div>"
+     
+    return divQuestions
+
 # Prompts user for a question/answer to the trace. The answers are stored in a json file.
 # returns question - returns the question and answer.
 def generateManualQuestion():
-    global answers
+    global answers, py
     question = checkEmptyInput("What is the manual question?")
 
     # Error checking to make sure the value is an integer.
