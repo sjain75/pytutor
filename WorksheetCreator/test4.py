c89cc5ecb858af33f8577c19ceba82109ea0d89d
ericzhzhou2001@gmail.com <ericzhzhou2001@gmail.com>
2020-08-16 13:57:44 -0400

    updated test4.py for indepth tracing

--- WorksheetCreator/test4.py
+++ WorksheetCreator/test4.py
@@ -2,4 +2,10 @@ x = """HELLO
 WILL I PRINT?
 I HOPEFULLY WILL!"""
 
+def printTriple():
+    print("""HELLO 
+    THIS SHOULD PRINT!
+    THIS Hopefully WILL PRINT!""")
 print(x)
+printTriple()
+print("""WHICH WILL PRINT FIRST""")
