a7e8c24a6af655fd63227882ece99384f266873e
sjain75 <sjain75@wisc.edu>
2021-01-11 10:38:54 -0600

    Ajax works with csrf token

--- django_pytutor/djangoPytutor/static/temp.js
+++ django_pytutor/djangoPytutor/static/temp.js
@@ -1,13 +1,33 @@
+var request=null;
+var csrftoken=null;
+$(document).ready(function(){
+csrftoken = $('[name=csrfmiddlewaretoken]')[0].value;
+
+request = new Request(
+    "template",
+    {headers: {'X-CSRFToken': csrftoken}}
+);
+});
 function convert() {
 	let code = $("#demoCode").val();
 
-	$.ajax({
-	  type: "POST",
-	  url: "http://127.0.0.1:8000/template",
-	  data: "{'hi':1}",
-	  success: success,
-	  dataType: "json"
+	fetch(request, {
+	    method: 'POST',
+	    mode: 'same-origin',  // Do not send CSRF token to another domain.
+		data: JSON.stringify({"code":code})
+	}).then(function(response) {
+	    console.log(response);
+		console.log(response.body);
+
 	});
+
+	// $.ajax({
+	//   type: "POST",
+	//   url: "http://127.0.0.1:8000/template",
+	//   data: {CSRF: csrftoken, 'hi':1},
+	//   success: success,
+	//   dataType: "json"
+	// });
 
 /*	$.post({
 	  type: "POST",
